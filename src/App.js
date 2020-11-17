53e36f3e205d1ae637bf8d75973f00030f31464b
Shruthi Sagar CR <sagar.s@digicollect.com>
2020-11-17 08:20:22 +0530

    #in progress routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import HomePage from "./pages/homepage/homepage.component"
 
+
+const HatsPage = ()=>(
+  <div>
+    <h1>Hats</h1>
+  </div>
+)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+ 
